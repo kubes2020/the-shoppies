aee368c92a4fda57d27f8189bb928791951e3ca4
Brian Kubes <briankubes@gmail.com>
2021-05-05 10:58:31 -0700

    remove nomination is working, disable nominate button is almost working

--- the-shoppies/src/components/MovieCard.js
+++ the-shoppies/src/components/MovieCard.js
@@ -9,34 +9,89 @@ function MovieCard(props) {
     props.saveNomination({ title: title, year: year });
   };
 
-  return (
-    <div className="stack-center">
-      {props.moviesSaved.map((mov, index) => {
-        return (
-          <div className="movie-card" key={index}>
-            <div className="movie-image">
-              <img src={mov.Poster} alt="movie pic"></img>
-            </div>
-            <div className="movie-text">
-              <div>{mov.Title}</div>
-              <div>{mov.Year}</div>
+  // let x = [1, 2, 3, 4];
+
+  // const renderCard = () => {
+  //   return props.moviesSaved.map((item, index) => {
+  //     return <div>{item.Title}</div>;
+  //   });
+  // };
+  let nominatedBool = false;
+
+  const renderCard = () => {
+    return props.moviesSaved.map((mov, index) => {
+      return (
+        <div className="movie-card" key={index}>
+          <div className="movie-image">
+            <img src={mov.Poster} alt="movie pic"></img>
+          </div>
+          <div className="movie-text">
+            <div>{mov.Title}</div>
+            <div>{mov.Year}</div>
+            {props.nominatedMovies.forEach((nom) => {
+              nominatedBool = nom.title === mov.Title;
+            })}
+            {nominatedBool ? null : (
               <button
                 className="nominate-button"
                 onClick={() => handleClick(mov.Title, mov.Year)}
               >
                 Nominate
               </button>
-            </div>
+            )}
           </div>
-        );
-      })}
-    </div>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <>
+      <h2>Search Results</h2>
+      <div className="stack-center">
+        <div>{renderCard()}</div>
+      </div>
+    </>
   );
 }
 
 const mapStateToProps = (state) => {
   return {
     moviesSaved: state.movies,
+    nominatedMovies: state.nominations,
   };
 };
 export default connect(mapStateToProps, { saveNomination })(MovieCard);
+
+// return (
+//   <>
+//     <h2>Search Results</h2>
+//     <div className="stack-center">
+//       {props.moviesSaved.map((mov, index) => {
+//         return (
+//           <div className="movie-card" key={index}>
+//             <div className="movie-image">
+//               <img src={mov.Poster} alt="movie pic"></img>
+//             </div>
+//             <div className="movie-text">
+//               <div>{mov.Title}</div>
+//               <div>{mov.Year}</div>
+//               {props.nominatedMovies.forEach((nom) => {
+//                 console.log("checking bool", nom.title === mov.Title);
+//                 let x = nom.title === mov.Title;
+//                 return x;
+//               })}
+
+//               <button
+//                 className="nominate-button"
+//                 onClick={() => handleClick(mov.Title, mov.Year)}
+//               >
+//                 Nominate
+//               </button>
+//             </div>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   </>
+// );
